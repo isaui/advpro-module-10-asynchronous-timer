46fb0ab430afe41d6d6c6aaea819cebe2c0b953d
isaui <isadestroyed17@gmail.com>
2024-05-05 22:02:29 +0700

    Experiment 1.2: Understanding how it works.

--- src/main.rs
+++ src/main.rs
@@ -104,6 +104,7 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Isa's Computer: done!");
     });
+    println!("Isa's Computer: hey hey");
 
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
